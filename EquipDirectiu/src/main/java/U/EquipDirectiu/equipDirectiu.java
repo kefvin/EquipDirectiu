c297bb1915365905be3faf96f1218dd9a1b89087
alumne1daw <alumne1daw@localhost>
2016-02-23 20:09:25 +0100

    Solo esta creado

---
+++ EquipDirectiu/src/main/java/U/EquipDirectiu/equipDirectiu.java
@@ -0,0 +1,16 @@
+package U.EquipDirectiu;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class equipDirectiu extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+		
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
